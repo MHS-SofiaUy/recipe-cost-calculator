b35952756624c009267b1eea0266880e3a733573
MHS-SofiaUy <86634615+MHS-SofiaUy@users.noreply.github.com>
2023-05-02 09:46:05 +1200

    Add files via upload

---
+++ 01_instructions.py
@@ -0,0 +1,29 @@
+# import libraries
+import pandas
+
+
+# functions go here
+
+# instructions go here
+def show_instructions():
+    print('''\n
+***** Instructions *****
+
+This program will ask you for ...
+- The name of the recipe you are using
+- The names of the ingredients you are using
+- How much of it you plan on using
+- The costs for the amount of servings you plan to make
+- How much it will cost per serving
+
+It will then output an itemised list of the costs 
+with subtotals for the variable and fixed costs.
+Finally it will tell you how much you should use of 
+each item for you to reach your serving goal.
+
+The data will also be written to a text file which
+has the same name as your recipe.
+
+**** Program Launched! ****''')
+
+
