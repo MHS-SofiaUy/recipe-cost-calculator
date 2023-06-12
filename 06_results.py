67bfc6d50788ac497c577014d6ad11ab02b0d3ac
MHS-SofiaUy <86634615+MHS-SofiaUy@users.noreply.github.com>
2023-06-13 09:49:14 +1200

    Add files via upload

--- 06_results.py
+++ 06_results.py
@@ -1,6 +1,6 @@
 # import libraries
 import pandas
-import math
+
 
 # functions go here
 
@@ -27,6 +27,8 @@ has the same name as your recipe.
 **** Program Launched! ****''')
 
 
+# *** functions go here ***
+
 # checks that input is either a float or an
 # integer that is more than zero. takes in custom error messages
 def num_check(question, error, num_type):
@@ -44,6 +46,7 @@ def num_check(question, error, num_type):
         except ValueError:
             print(error)
 
+
 def string_checker(question, valid_responses):
     error = "Please choose one of the following options: {}".format(", ".join(valid_responses))
 
@@ -56,6 +59,7 @@ def string_checker(question, valid_responses):
 
         print(error)
 
+
 def not_blank(question, error):
     valid = False
     while not valid:
@@ -66,31 +70,14 @@ def not_blank(question, error):
         else:
             return response
 
-# rounding function
-def round_up(amount, rounding_to):
-    return int(math.ceil(amount / rounding_to)) * rounding_to
+
+# currency formatting function
+def currency(x):
+    return "${:.2f}".format(x)
 
 
-# *** functions go here ***
-
-# list
-yes_no_list = ["yes", "no"]
-texture_list = ["dry", "wet"]
-
-# dictionaries for units
-measurement_list = {
-    "grams": "g",
-    "kilograms": "kg"
-}
-measurement_list_wet = {
-    "millilitres": "ml",
-    "litres": "l"
-}
-# ask user if they want to see the instructions
-want_instructions = string_checker("do you want to read the instructions (y/n)?: ", yes_no_list)
-
-if want_instructions == "yes":
-    print(show_instructions())
+# import libraries
+# CHATGPT
 
 def recipe_cost_calculator():
     global wet_measurement, dry_measurement
@@ -107,42 +94,76 @@ def recipe_cost_calculator():
         # ...
     else:
         print("Invalid input. Please enter 'wet' or 'dry'.")
-# *** main routine goes here ***
+
+# *** functions go here ***
+
+# list
+yes_no_list = ["yes", "no"]
+texture_list = ["dry", "wet"]
+
+# ask user if they want to see the instructions
+want_instructions = string_checker("do you want to read the instructions (y/n)?: ", yes_no_list)
+
+if want_instructions == "yes":
+    print(show_instructions())
 
 # get recipe name
 recipe_name = not_blank("Recipe name: ", "The recipe name can't be blank.")
 
-# get ingredient name
-ingredient_name = not_blank("Ingredient: ", "The component can't be blank.")
-wet_measurement = ""
-dry_measurement = ""
+# loop to get component, quantity and price
+ingredient_name = ""
+while ingredient_name.lower() != "xxx":
+    # get ingredient name
+    ingredient_name = not_blank("\nIngredient: ", "The component can't be blank.")
 
-# Call the function to start the recipe cost calculator
-recipe_cost_calculator()
+    # if
+    if ingredient_name.lower() == "xxx":
+        break
 
-get_int = num_check("How much did you get of this ingredient? ", "Please enter an amount more than 0\n", float)
-get_cost = num_check("How much does it cost (for the amount you bought)? $", "Please enter a number more than 0\n",
-                     float)
-get_amount = num_check("How much are you using in the recipe? ", "Please enter an amount more than 0\n", float)
+    # dictionaries for units
+    measurement_list = {
+        "grams": "g",
+        "kilograms": "kg"
+    }
+    measurement_list_wet = {
+        "millilitres": "ml",
+        "litres": "l"
+    }
+
+    # get
+    measurement_unit = ""
+    wet_measurement = ""
+    dry_measurement = ""
+
+    # Call the function to start the recipe cost calculator
+    recipe_cost_calculator()
+
+    if wet_measurement:
+        measurement_unit = measurement_list_wet[wet_measurement]
+    elif dry_measurement:
+        measurement_unit = measurement_list[dry_measurement]
+    else:
+        print("Invalid input. Please try again.")
+
+    get_int = num_check("How much did you get of this ingredient? ", "Please enter an amount more than 0\n", float)
+    get_cost = num_check("How much does it cost (for the amount you bought)? $", "Please enter a number more than 0\n",
+                        float)
+    get_amount = num_check("How much are you using in the recipe? ", "Please enter an amount more than 0\n", float)
+        
+
 
 # to get the price
 get_price = get_cost / get_int * get_amount
 
-# calculations for recipe
-print(" ----- {} ----- ".format(recipe_name))
 
-measurement_unit = ""
+# main routine goes here
 
-if wet_measurement:
-    measurement_unit = measurement_list_wet[wet_measurement]
-elif dry_measurement:
-    measurement_unit = measurement_list[dry_measurement]
-else:
-    print("Invalid input. Please try again.")
 
-if measurement_unit:
-    print("You need: {}{}".format(get_amount, measurement_unit))
-else:
-    print("Invalid input. Please try again.")
+
+# calculating the price
+print("----- {} -----".format(recipe_name))
+print("{} - ${}".format(ingredient_name, get_price))
+
+
 
 print("It costs: ${:.2f}".format(get_price))
